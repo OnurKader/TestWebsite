03fa185dd457dc828d188961d78729327f89e90e
OnurKader <onurorkunkader1999@gmail.com>
2019-03-18 01:14:54 +0300

    Dott

--- sketch.js
+++ sketch.js
@@ -16,7 +16,18 @@ function mouseDragged() {
   line(mouseX, mouseY, pmouseX, pmouseY);
 }
 
+function mousePressed() {
+  let r = map(mouseY, 0, height, 254, 25);
+  let g = map(mouseX, 0, width, 25, 254);
+  let b = map(seconds(), 0, 59, 66, 212);
+  stroke(r, g, b, 1);
+  noFill();
+  strokeWeight(144);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
 
 function draw() {
+  stroke(255);
+  strokeWeight(30);
   point(mouseX, mouseY);
 }
