44d81329a12d6c651ae14bc28557e3d119c10a00
OnurKader <onurorkunkader1999@gmail.com>
2019-03-18 01:23:34 +0300

    console.log

--- sketch.js
+++ sketch.js
@@ -3,11 +3,12 @@ function setup() {
   background(0);
   fill(236, 102, 121);
   noStroke();
-  rect(width / 2, height / 2, 80, 175);
+  rect(width / 2, height / 2 - 50, 80, 175);
 }
 
 function draw() {
   if (mousePressed) {
+    console.log("Hey!");
     stroke(map(mouseY, 0, height, 252, 44), map(mouseX, 0, width, 42, 253), 189);
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
