63e94ca2d4be5d4b6f634bb03b364806277ff514
OnurKader <onurorkunkader1999@gmail.com>
2019-03-18 01:07:14 +0300

    Added a Cute Drawing Tool

---
+++ sketch.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(1280, 720);
+  background(0);
+
+
+}
+
+function mouseDragged() {
+  let r = map(mouseY, 0, height, 254, 25);
+  let g = map(mouseX, 0, width, 25, 254);
+  let b = map(seconds(), 0, 59, 66, 212);
+  stroke(r, g, b, 1);
+  noFill();
+  strokeWeight(144);
+  line(mouseX, mouseY, pmouseX, pmouseY);
+}
+
+
+function draw() {
+
+}
