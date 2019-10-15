aacbebec0d543ef75986ec6f4bb4e9d2249aff55
OnurKader <onurorkunkader1999@gmail.com>
2019-10-15 17:33:37 +0100

    Updated the libraries

--- sketch.js
+++ sketch.js
@@ -1,12 +1,12 @@
 function setup() {
-  createCanvas(displayWidth, displayHeight);
-  background(0);
+	createCanvas(displayWidth, displayHeight);
+	background(0);
+	stroke(map(mouseY, 0, height, 252, 44), map(mouseX, 0, width, 42, 253), 189, 1);
+	strokeWeight(144);
 }
 
 function draw() {
   if (mouseIsPressed) {
-    stroke(map(mouseY, 0, height, 252, 44), map(mouseX, 0, width, 42, 253), 189, 1);
-    strokeWeight(144);
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
