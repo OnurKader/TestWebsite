f81082e6a3778cacef4dd8b297686889c2f8e82d
OnurKader <onurorkunkader1999@gmail.com>
2019-10-23 17:41:41 +0100

    Switched to HSV

--- sketch.js
+++ sketch.js
@@ -2,11 +2,12 @@ function setup() {
 	createCanvas(displayWidth, displayHeight);
 	background(0);
 	strokeWeight(144);
+	colorMode(HSV);
 }
 
 function draw() {
   if (mouseIsPressed) {
-	stroke(map(mouseY, 0, height, 252, 44), map(mouseX, 0, width, 42, 253), 189, 1);
+	stroke(map(mouseY, 0, height, 252, 44), map(mouseX, 0, width, 42, 253), 255, 1);
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
