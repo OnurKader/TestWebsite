989fb5c89ade6406055b3ea263ea54464e5d4616
Onur Orkun KADER <33760623+OnurKader@users.noreply.github.com>
2019-11-04 21:54:24 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,7 +7,7 @@ function setup() {
 
 function draw() {
   if (mouseIsPressed) {
-	stroke(map(mouseY, 0, height, 358, 46), map(mouseX, 0, width, 41, 352), 360, 2);
+	stroke(map(mouseY, 0, height, 356, 13), map(mouseX, 0, width, 21, 358), 360, 2);
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
