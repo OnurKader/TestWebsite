d539db1b6f56f2db223cbefbe395aa02041d1759
Onur Orkun KADER <33760623+OnurKader@users.noreply.github.com>
2019-11-04 21:52:21 +0000

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,12 +2,12 @@ function setup() {
 	createCanvas(displayWidth, displayHeight);
 	background(0);
 	strokeWeight(144);
-	colorMode(HSB);
+	colorMode(HSB, 360);
 }
 
 function draw() {
   if (mouseIsPressed) {
-	stroke(map(mouseY, 0, height, 252, 44), map(mouseX, 0, width, 42, 253), 255, 1);
+	stroke(map(mouseY, 0, height, 358, 46), map(mouseX, 0, width, 41, 352), 360, 10);
     line(mouseX, mouseY, pmouseX, pmouseY);
   }
 }
